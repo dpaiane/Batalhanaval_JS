1bc6976191adf4c536f1f33536b548f2dc4f0a46
Diego paiani Cardoso <73446426+dpaiane@users.noreply.github.com>
2024-09-26 18:40:24 -0300

    Update TesteBN.js

--- TesteBN.js
+++ TesteBN.js
@@ -1,6 +1,8 @@
 const teclado = require('prompt-sync')({sigint:true});
 // Condição que faz reiniciar o jogo
-while(Fim_jogo){
+Fim_jogo = false;
+
+while(Fim_jogo == false){
     let Tabuleiro = [];
     let Qtd_Barcos = 10;
     let Qtd_Jogadas = 12;
@@ -8,7 +10,7 @@ while(Fim_jogo){
     let JogadaX = 0;
     let JogadaY = 0;    
     let Pontos = 0;
-    let Fim_jogo = false;
+    let Ganhador = 0;
 
     for (let i = 0; i < 5; i++) {
         Tabuleiro[i] = [];
@@ -16,7 +18,7 @@ while(Fim_jogo){
             Tabuleiro[i][j] = false;
         }
     }
-    console.log(Tabuleiro);
+    //console.log(Tabuleiro);
     for(let i=0;i<Qtd_Barcos;i++){
         let posX = parseInt(Math.floor(Math.random() * 5));
         let posY = parseInt(Math.floor(Math.random() * 5));  
@@ -27,15 +29,24 @@ while(Fim_jogo){
     for(let i = 0; i < Qtd_Jogadas; i++){
         JogadaX = parseInt(teclado("digite a posição do tiro em x: "));
         JogadaY = parseInt(teclado("digite a posição do tiro em y: "));
+        if(!isNaN(JogadaX) || !isNaN(JogadaY)){
+            console.log("digite um número válido");
+            JogadaX = parseInt(teclado("digite a posição do tiro em x: "));
+            JogadaY = parseInt(teclado("digite a posição do tiro em y: "));
+        }
         Qtd_Tiros--;
     
         if(Tabuleiro[JogadaX][JogadaY] === true){
+            console.log("*************************************");
             console.log("********  BARCO AFUNDADO ********");
+            console.log("*************************************");
             Tabuleiro[JogadaX][JogadaY] = false;
             Pontos += 10; 
         }
         else if(Tabuleiro[JogadaX][JogadaY] === false){
+            console.log("*************************************");
             console.log("********  TIRO NA AGUA ********");
+            console.log("*************************************");
         }
         console.log("*************************************");
         console.log("** Quantidade tiros:",Qtd_Tiros);
@@ -43,9 +54,32 @@ while(Fim_jogo){
         console.log("*************************************");
         // Se os tiros acabaram o jogo deve ser encerrado
         if(Qtd_Tiros == 0){
-            let Restart = teclado("**Digite n para jogar novamente**");
-            if(Restart == true){
-                Fim_jogo = true
+            console.log("*************************************");
+            console.log("** PARABENS VOCÊ VENCEU **");
+            console.log("*************************************");
+            let Continuar = teclado("** Digite n para jogar novamente ou qualque tecla para sair **");
+            if(Continuar == "n"){
+                Fim_jogo = false;
+                if(Pontos > 60){
+                    console.log("*************************************");
+                    console.log("** PARABENS VOCÊ VENCEU **");
+                    console.log("*************************************");
+                }else{
+                    console.log("*************************************");
+                    console.log("** QUE PENA VOCÊ PERDEU **");
+                    console.log("*************************************");
+                }
+            }else{
+                if(Ganhador > 6){
+                    console.log("*************************************");
+                    console.log("** PARABENS VOCE VENCEU **");
+                    console.log("*************************************");
+                }else{
+                    console.log("*************************************");
+                    console.log("** QUE PENA VOCÊ PERDEU **");
+                    console.log("*************************************");
+                }
+                Fim_jogo = true;
             }
         }
     }
